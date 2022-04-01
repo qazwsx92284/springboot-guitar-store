a90be2328c7e36267b29a3a8652542ef5bba04b1
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-03-31 22:11:00 -0400

    added ItemRequestValidatorTest invalid case

--- src/test/java/com/my/guitarstore/ReadJsonFile.java
+++ src/test/java/com/my/guitarstore/ReadJsonFile.java
@@ -2,6 +2,7 @@ package com.my.guitarstore;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.guitarstore.model.Item;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,11 +14,11 @@ public class ReadJsonFile {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-//    public static List<Item> getItemFromJson() throws IOException {
-//        InputStream inputStream = new FileInputStream(new File("src/test/java/resources/ItemList.json"));
-//        TypeReference<List<Item>> typeReference = new TypeReference<List<Item>>() {};
-//        return objectMapper.readValue(inputStream, typeReference);
-//    }
+    public static List<Item> getItemFromJson() throws IOException {
+        InputStream inputStream = new FileInputStream(new File("src/test/java/resources/ItemList.json"));
+        TypeReference<List<Item>> typeReference = new TypeReference<List<Item>>() {};
+        return objectMapper.readValue(inputStream, typeReference);
+    }
 
     /**
         refactored above method to return any POJO object by passing json path.
--- src/test/java/com/my/guitarstore/util/ItemRequestValidatorTest.java
+++ src/test/java/com/my/guitarstore/util/ItemRequestValidatorTest.java
@@ -1,5 +1,6 @@
 package com.my.guitarstore.util;
 
+import com.my.guitarstore.ReadJsonFile;
 import com.my.guitarstore.model.Item;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -18,13 +20,19 @@ class ItemRequestValidatorTest {
 
 
     @Test
-    void validateAddRequestTest() throws IOException {
+    void validateAddRequestTest_Valid() {
         Item item = getItem();
 //        Object object = null;
 //        Item itemCast = (Item)object;
 //        itemCast = (Item) ReadJsonFile.getListFromJson("src/test/java/resources/ItemList.json").get(0);
         boolean result = itemRequestValidator.validateAddRequest(item);
         assertTrue(result);
+    }
+
+    @Test
+    void validateAddRequestTest_Invalid() throws IOException {
+        Item item = ReadJsonFile.getItemFromJson().get(1);
+        assertFalse(itemRequestValidator.validateAddRequest(item));
     }
 
     private Item getItem() {
