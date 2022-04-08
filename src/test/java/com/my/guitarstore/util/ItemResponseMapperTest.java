d2990fe4eae299b5efd1fcf2a32ea29549c09916
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-04-07 23:55:59 -0400

    added calculateFinalPriceTest

--- src/test/java/com/my/guitarstore/util/ItemResponseMapperTest.java
+++ src/test/java/com/my/guitarstore/util/ItemResponseMapperTest.java
@@ -2,6 +2,7 @@ package com.my.guitarstore.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.my.guitarstore.ReadJsonFile;
+import com.my.guitarstore.model.Discount;
 import com.my.guitarstore.model.Item;
 import com.my.guitarstore.response.ItemRO;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,13 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class ItemResponseMapperTest {
@@ -35,6 +41,16 @@ class ItemResponseMapperTest {
         assertEquals(item.getAmount(), mockResult.getAmount());
         assertEquals(item.getName(), mockResult.getName());
         assertEquals(item.getRegularPrice(), mockResult.getRegularPrice());
+    }
+
+    @Test
+    void calculateFinalPriceTest() {
+        Map<String, Integer> discountMap = new HashMap();
+        when(objectMapper.convertValue(any(), eq(Map.class))).thenReturn(discountMap);
+        Discount discount = new Discount();
+        discount.setCouponDiscount(20);
+        Double mockResult = itemResponseMapper.calculateFinalPrice(discount, 20d);
+        assertEquals(16d, mockResult);
     }
 
 
